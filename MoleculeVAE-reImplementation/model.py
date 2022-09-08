ead49d75fbe06b1561152fa45aaffdeb143ba07c
timcanby <kangyinglee0501@gmail.com>
2022-09-08 20:33:08 +0900

    Update model 2022.9.8

--- MoleculeVAE-reImplementation/model.py
+++ MoleculeVAE-reImplementation/model.py
@@ -1,21 +1,10 @@
-import torch
+
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data
-import gzip
-import pandas
-import numpy as np
-import argparse
-import os
-import h5py
-import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from sklearn import model_selection
-import zipfile
+
 import torch
-from sklearn.preprocessing import OneHotEncoder
-import h5py
-import numpy as np
+
 
 
 class MolecularVAE(nn.Module):
@@ -24,14 +13,14 @@ class MolecularVAE(nn.Module):
         self.conv_1 = nn.Conv1d(120, 9, kernel_size=9)
         self.conv_2 = nn.Conv1d(9, 9, kernel_size=9)
         self.conv_3 = nn.Conv1d(9, 70, kernel_size=8)
-        self.linear_0 = nn.Linear(210, 435)
+        self.linear_0 = nn.Linear(840, 435)
         self.linear_1 = nn.Linear(435, 292)
         self.linear_2 = nn.Linear(435, 292)
         self.linear_3 = nn.Linear(292, 292)
         self.linear_p1 = nn.Linear(292, 100)
         self.linear_p2 = nn.Linear(100, 3)
         self.gru = nn.GRU(292, 501, 3, batch_first=True)
-        self.linear_4 = nn.Linear(501, 26)
+        self.linear_4 = nn.Linear(501, 35)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax()
 
@@ -54,8 +43,9 @@ class MolecularVAE(nn.Module):
         out_reshape = output.contiguous().view(-1, output.size(-1))
         y0 = F.softmax(self.linear_4(out_reshape), dim=1)
         y = y0.contiguous().view(output.size(0), -1, y0.size(-1))
+
         return y
-    def PrePro(self,z):
+    def PredictProperties(self,z):
         z = F.selu(self.linear_3(z))
         z=F.selu(self.linear_p1(z))
         z=self.linear_p2(z)
@@ -64,4 +54,4 @@ class MolecularVAE(nn.Module):
     def forward(self, x):
         z_mean, z_logvar = self.encode(x)
         z = self.sampling(z_mean, z_logvar)
-        return self.decode(z), z_mean, z_logvar,self.PrePro(z)
+        return self.decode(z), z_mean, z_logvar,self.PredictProperties(z)
