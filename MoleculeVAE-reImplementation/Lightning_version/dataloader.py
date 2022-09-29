d65c9906be0bf4da05e0a88fe36626694dc17538
timcanby <kangyinglee0501@gmail.com>
2022-09-30 04:21:44 +0900

    Update_Lightning_version
    Update_Lightning_version

---
+++ MoleculeVAE-reImplementation/Lightning_version/dataloader.py
@@ -0,0 +1,266 @@
+from rdkit import rdBase, Chem
+
+print(rdBase.rdkitVersion)
+import pandas as pd
+import os
+import sys
+import hyperparameters
+
+sys.path.append(os.path.join(Chem.RDConfig.RDContribDir, "SA_Score"))
+import torch
+import torch.nn.functional as F
+import torch.utils.data
+import sascorer
+import argparse
+import json
+import collections
+import numpy as np
+from sklearn.model_selection import train_test_split
+from rdkit.Chem import QED
+import deepchem as dc
+import ast
+
+'''
+**Constant:ALL_SMILES_CHARACTERS
+Unlike dictionary used by https://github.com/aksub99/molecular-vae 
+
+Authors mentioned their SMILES-based text encoding used a subset of
+35 different characters for ZINC and 22 different characters for QM9. 
+Actually there are more than 24 characters in QM9 Dataset.
+
+In Update version using 35 characters
+'''
+ALL_SMILES_CHARACTERS = ["S", "B", "\n", "P", "3", "s",
+                        "O", "=", ")", "l", "r", "[",
+                        "5", "2", "\\", "F", "]", "1",
+                        "N", "8", "-", "c", "4", "6",
+                        "#", "n", "/", "+", "o", "I",
+                        "C", "H", "@", "(", "7", "."]
+
+'''
+Read in data:
+This project provides two ways to read files, one is to read from a local file(.csv),
+the other is to read from Molecule Net (https://moleculenet.org/)
+From local:load_dataset_from_file()
+From MoleculeNet:load_dataset_from_deepchem():
+
+Use load_dataset() to load them
+'''
+"""
+Read in data:
+This project provides two ways to read files, one is to read from a local file(.csv),
+the other is to read from Molecule Net (https://moleculenet.org/)
+From local:load_dataset_from_file()
+From MoleculeNet:load_dataset_from_deepchem():
+
+Use load_dataset() to load them
+"""
+
+
+def load_dataset_from_file(data_path):
+    """Loads dataset from local file.
+    - Inputs:
+        -path: 'Data/250k_rndm_zinc_drugs_clean_3.csv' or user-defined
+    - Outputs:
+        - dataset: type=pandas object
+    """
+    return pd.read_csv(data_path)
+
+
+def load_dataset_from_deepchem(featurizer):
+    """Loads dataset from deepcm Default QM9.
+    -
+        -featurizer: whether use dataset from moleculeNet type=Boolean value default=True
+    - Outputs:
+        - dataset: type=list
+    """
+    _, datasets, _ = dc.molnet.load_qm9(featurizer=featurizer)
+    return datasets
+
+
+def load_dataset(params):
+    """Loads dataset, either from deepchem or from local file system.
+    - Inputs:
+        -dataset:path to local dataset
+        -from_deepchem: whether use dataset from moleculeNet type=Boolean value default=True
+        -featurizer: default='ECFP'
+    - Outputs:
+        - dataset: type=list
+        This is to load dataset for training"""
+    if params["FromDeepchem"]:
+        datasets = load_dataset_from_deepchem(params["featurizer"])
+        train_dataset, valid_dataset, test_dataset = datasets
+        """train_dataset.ids=smile string of molecule (ref:https://deepchem.readthedocs.io/en/latest/api_reference/moleculenet.html)"""
+        smi_onehot_train, character_index_lookup_dict = one_hot_encoder(
+            train_dataset.ids, params["normalizeSize"]
+        )
+        smi_onehot_test, character_index_lookup_dict = one_hot_encoder(
+            test_dataset.ids, params["normalizeSize"]
+        )
+        if params["do_prop_pred"]:
+            X_train, Y_train = extract_properties_value(
+                smi_onehot_train, train_dataset.ids
+            )
+            X_test, Y_test = extract_properties_value(smi_onehot_test, test_dataset.ids)
+            return X_train, X_test, Y_train, Y_test, character_index_lookup_dict
+        else:
+            return smi_onehot_train, smi_onehot_test, character_index_lookup_dict
+    else:
+        datasets = load_dataset_from_file(data_path=params["Data_file"])
+        train_docs = datasets["smiles"].tolist()
+        logP = datasets["logP"].tolist()
+        QED = datasets["qed"].tolist()
+        SAS = datasets["SAS"].tolist()
+        train_docsy = np.stack((logP, QED, SAS), axis=-1)
+        X_train_s, X_test_s, Y_train, Y_test = train_test_split(train_docs, train_docsy)
+        X_train, character_index_lookup_dict = one_hot_encoder(
+            X_train_s, params["normalizeSize"]
+        )
+        X_test, character_index_lookup_dict = one_hot_encoder(
+            X_test_s, params["normalizeSize"]
+        )
+        if params["do_prop_pred"]:
+
+
+            return X_train, X_test, Y_train, Y_test, character_index_lookup_dict
+        else:
+            return X_train, X_test, character_index_lookup_dict
+
+
+
+def one_hot_encoder(smile_data, normalize_size):
+    """This is one hot embedding function
+    - Inputs:
+            -smile_data:list of smile string
+            -normalize_size: the maximum number of characters in a single smile string
+        - Outputs:
+            - one_hot_feature: one hot vector
+            - character_index_lookup_dict:"""
+    character_index_lookup_dict = load_character_index_lookup_dict()
+    one_hot_feature = [
+        make_encoding_for_smiles(vec, character_index_lookup_dict, normalize_size)
+        for vec in smile_data
+    ]
+    return one_hot_feature, character_index_lookup_dict
+
+
+def load_character_index_lookup_dict():
+    """This is a dictionary that maps character to a unique index"""
+    character_index_lookup_dict = collections.OrderedDict(
+        [(a, i) for i, a in enumerate(ALL_SMILES_CHARACTERS)])
+    return character_index_lookup_dict
+
+
+def make_encoding_for_smiles(string_list, character_index_lookup_dict, normalize_size):
+    """This is to convert string to one hot vector
+    - Inputs:
+        -string_list:list of smile string
+        -character_index_lookup_dict: a dictionary that maps character to a unique index
+        -normalize_size: the maximum number of characters in a single smile string
+    - Outputs:
+        - onehot_out: one hot vector
+    """
+
+    index_list = list(map(lambda a: character_index_lookup_dict[a], [*string_list]))
+    num_classes = len(character_index_lookup_dict)
+    one_hot_vector = torch.nn.functional.one_hot(
+        torch.tensor(index_list), num_classes=num_classes
+    )
+    padding_size = (
+        0,
+        0,
+        0,
+        normalize_size - len(string_list),
+    )  # pad last dim by 1 on each side
+    onehot_out = F.pad(one_hot_vector, padding_size, "constant", 0)
+    """output shape=[normalize_size,len(character_index_lookup_dict)]"""
+    return onehot_out
+
+
+
+def caculate_target_values(smiles):
+    """Calculates logP, QED and SAS for a smiles input;
+    - Inputs:
+        - smiles:
+          str. The smiles of the molecule for which we want to caluclate the values.
+    - Returns:
+        - logP:
+          water−octanol partition coefficient (logP) ref.
+        - QED
+          synthetic accessibility score (SAS)
+        - SAS
+          Estimation of Drug-likeness (QED)
+    If the molecule cannot be calculated by RDkit, returns error
+    These values calculated here sometimes disagree with the values given in the dataset from the original
+    dataset.
+    """
+    m = Chem.MolFromSmiles(smiles)
+    logp = Chem.Descriptors.MolLogP(m)
+    qed = QED.default(m)
+    sas = sascorer.calculateScore(m)
+
+    return logp, qed, sas
+
+
+def extract_properties_value(smile_onehot, smile_string):
+    """Extract properties value for latent space prediction
+    - Inputs:
+        - smile_onehot: one hot vector for training.
+        - smile_string: corresponding smile string.
+    - Returns:
+        - X_final: one hot vector which has the corresponding properties value
+        - Y_values:properties value
+    """
+    X_final = []
+    Y_values = []
+    label = []
+    if os.path.exists("properties_value.json"):
+        with open("properties_value.json", "r", encoding="utf-8") as json_file:
+            properties_dic = ast.literal_eval(json.load(json_file))
+
+        for x, s_string in zip(smile_onehot, smile_string):
+            print(s_string)
+            if str(s_string) in properties_dic:
+                y = properties_dic[str(s_string)]
+            else:
+                try:
+                    y = caculate_target_values(s_string)
+                    temp_result = {str(s_string): y}
+                    properties_dic.update(temp_result)
+                except:
+                    continue
+
+            X_final.append(x)
+            Y_values.append(y)
+            label.append(s_string)
+    else:
+        for x, s_string in zip(smile_onehot, smile_string):
+            try:
+                y = caculate_target_values(s_string)
+            except:
+                continue
+            X_final.append(x)
+            Y_values.append(y)
+            label.append(s_string)
+            properties_dic = dict(zip(label, Y_values))
+    """creat & update properties_value.json"""
+    with open("properties_value.json", "w") as outfile:
+        json.dump(str(properties_dic), outfile)
+    return X_final, Y_values
+def one_hot_decoder(one_hot_data,dic):
+    dic_swap = {v: k for k, v in dic.items()}
+    return["".join(map(str, list(map(lambda a: dic_swap[a], ids.argmax(-1))))) for ids in one_hot_data]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-e", "--exp_file", help="experiment file", default="expParam.json"
+    )
+    parser.add_argument("-d", "--directory", help="exp directory", default=None)
+    args = vars(parser.parse_args())
+    if args["directory"] is not None:
+        args["exp_file"] = os.path.join(args["directory"], args["exp_file"])
+
+    params = hyperparameters.load_params(args["exp_file"])
+    '''For checking and update the parameters'''
---
+++ MoleculeVAE-reImplementation/Lightning_version/model.py
@@ -0,0 +1,293 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+import gzip
+import pandas
+import numpy as np
+import argparse
+import os
+import h5py
+import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from sklearn import model_selection
+import zipfile
+import torch
+from sklearn.preprocessing import OneHotEncoder
+import h5py
+import numpy as np
+import pytorch_lightning as pl
+import hyperparameters
+from torch.utils.data import Dataset, DataLoader
+from dataloader import load_dataset
+import torch.utils.data as data
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import CSVLogger
+
+class MolecularVAE(pl.LightningModule):
+    def __init__(self):
+        super(MolecularVAE, self).__init__()
+        self.conv_1 = nn.Conv1d(120, 9, kernel_size=9)
+        self.conv_2 = nn.Conv1d(9, 9, kernel_size=9)
+        self.conv_3 = nn.Conv1d(9, 70, kernel_size=8)
+        self.linear_0 = nn.Linear(910, 435)
+        self.linear_1 = nn.Linear(435, 292)
+        self.linear_2 = nn.Linear(435, 292)
+        self.linear_3 = nn.Linear(292, 292)
+        self.linear_p1 = nn.Linear(292, 100)
+        self.linear_p2 = nn.Linear(100, 3)
+        self.visualize_z=nn.Linear(3,2)
+        self.gru = nn.GRU(292, 501, 3, batch_first=True)
+        self.linear_4 = nn.Linear(501, 36)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax()
+
+    def encode(self, x):
+        x = self.relu(self.conv_1(x))
+        x = self.relu(self.conv_2(x))
+        x = self.relu(self.conv_3(x))
+        x = x.view(x.size(0), -1)
+        x = F.selu(self.linear_0(x))
+        return self.linear_1(x), self.linear_2(x)
+
+    def sampling(self, z_mean, z_logvar):
+        epsilon = 1e-2 * torch.randn_like(z_logvar)
+        return torch.exp(0.5 * z_logvar) * epsilon + z_mean
+
+    def decode(self, z):
+        z = F.selu(self.linear_3(z))
+        z = z.view(z.size(0), 1, z.size(-1)).repeat(1, 120, 1)
+        output, hn = self.gru(z)
+        out_reshape = output.contiguous().view(-1, output.size(-1))
+        y0 = F.softmax(self.linear_4(out_reshape), dim=1)
+        y = y0.contiguous().view(output.size(0), -1, y0.size(-1))
+
+        return y
+    def predict_properties(self,z):
+        z = F.selu(self.linear_3(z))
+        z=F.selu(self.linear_p1(z))
+        z=self.linear_p2(z)
+        return z
+
+    def vae_loss(self,x_decoded_mean, x, z_mean, z_logvar):
+        xent_loss = nn.MSELoss()
+        vecloss = xent_loss(x_decoded_mean.to(dtype=torch.float32),
+                            x.to(dtype=torch.float32))
+        kl_loss = -0.5 * torch.sum(1 + z_logvar - z_mean.pow(2) - z_logvar.exp())
+        return vecloss + kl_loss
+
+    def vae_loss_with_annealing(self,x_decoded_mean, x, z_mean, z_logvar, epoch):
+        xent_loss = nn.MSELoss()
+        vecloss = xent_loss(x_decoded_mean.to(dtype=torch.float32),
+                            x.to(dtype=torch.float32))
+        kl_loss = -0.5 * torch.sum(1 + z_logvar - z_mean.pow(2) - z_logvar.exp())
+
+        if epoch >= params['vae_annealer_start']:
+            return vecloss + self.frange_cycle_sigmoid(0.0, 1.0, epoch, 4, 0.5)[0] * kl_loss
+        else:
+            return vecloss + kl_loss
+
+    '''============cyclical_annealing for KL vanishing problem==================
+    from paper: Cyclical Annealing Schedule: A Simple Approach to Mitigating KL Vanishing (NAACL 2019)
+    paper link:https://arxiv.org/abs/1903.10145
+    Github:https://github.com/haofuml/cyclical_annealing
+
+    KL vanishing:https://alibabatech.medium.com/next-gen-text-generation-alibaba-makes-progress-on-the-kl-vanishing-problem-77ec35f2afa2
+    '''
+
+    def frange_cycle_linear(self,start, stop, n_epoch, n_cycle=4, ratio=0.5):
+        L = np.ones(n_epoch)
+        period = n_epoch / n_cycle
+        step = (stop - start) / (period * ratio)  # linear schedule
+
+        for c in range(n_cycle):
+            v, i = start, 0
+            while v <= stop and (int(i + c * period) < n_epoch):
+                L[int(i + c * period)] = v
+                v += step
+                i += 1
+        return L
+
+    def frange_cycle_sigmoid(self,start, stop, n_epoch, n_cycle=4, ratio=0.5):
+        L = np.ones(n_epoch)
+        period = n_epoch / n_cycle
+        step = (stop - start) / (period * ratio)  # step is in [0,1]
+
+        # transform into [-6, 6] for plots: v*12.-6.
+
+        for c in range(n_cycle):
+            v, i = start, 0
+            while v <= stop:
+                L[int(i + c * period)] = 1.0 / (1.0 + np.exp(- (v * 12. - 6.)))
+                v += step
+                i += 1
+        return L
+
+    #  function  = 1 − cos(a), where a scans from 0 to pi/2
+
+    def frange_cycle_cosine(self,start, stop, n_epoch, n_cycle=4, ratio=0.5):
+        L = np.ones(n_epoch)
+        period = n_epoch / n_cycle
+        step = (stop - start) / (period * ratio)  # step is in [0,1]
+
+        # transform into [0, pi] for plots:
+
+        for c in range(n_cycle):
+            v, i = start, 0
+            while v <= stop:
+                L[int(i + c * period)] = 0.5 - .5 * math.cos(v * math.pi)
+                v += step
+                i += 1
+        return L
+
+    '''====End of cyclical_annealing module================================'''
+
+    def calculate_loss_by_type(self,decoder_output_data, input_string_data, z_mean, z_logvar, property_prediction_loss,
+                               epochs):
+        '''To calculate the loss value '''
+        pre_loss = property_prediction_loss
+        loss_type = params['loss_type']
+        if loss_type == "Variance_only":
+            loss = self.vae_loss(decoder_output_data, input_string_data, z_mean, z_logvar)
+        elif loss_type == "pro_prediction_only":
+            loss = -pre_loss
+        elif loss_type == "vae_pre_no_annealing":
+            loss = self.vae_loss(decoder_output_data, input_string_data, z_mean, z_logvar) + pre_loss
+        elif loss_type == "vae_pre_with_annealing":
+            loss = self.vae_loss_with_annealing(decoder_output_data, input_string_data, z_mean, z_logvar, epochs) + pre_loss
+        else:
+            loss = None
+        return loss
+
+    def forward(self, x):
+        z_mean, z_logvar = self.encode(x)
+        z = self.sampling(z_mean, z_logvar)
+        return self.decode(z), z_mean, z_logvar,self.predict_properties(z),z
+    def training_step(self,batch,batch_idx):
+        x,y=batch
+
+        output, mean, logvar, pre, z = self(x.to(dtype=torch.float32))
+        pre_loss_ca = nn.MSELoss()
+        pre_loss = pre_loss_ca(pre, y.to(dtype=torch.float32))
+        loss = self.calculate_loss_by_type(output, x, mean, logvar, pre_loss, batch_idx)
+        self.log("train_loss", loss)
+        return {"loss":loss}
+    def validation_step(self,batch,batch_idx):
+        x,y=batch
+
+        output, mean, logvar, pre, z = self(x.to(dtype=torch.float32))
+        pre_loss_ca = nn.MSELoss()
+        pre_loss = pre_loss_ca(pre, y.to(dtype=torch.float32))
+        val_loss = self.calculate_loss_by_type(output, x, mean, logvar, pre_loss, batch_idx)
+        self.log("val_loss", val_loss)
+        return {"val_loss":val_loss}
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        print(np.shape(x))
+        output, mean, logvar, pre, z = self(x.to(dtype=torch.float32))
+        pre_loss_ca = nn.MSELoss()
+        pre_loss = pre_loss_ca(pre, y.to(dtype=torch.float32))
+        test_loss = self.calculate_loss_by_type(output, x, mean, logvar, pre_loss, batch_idx)
+        self.log("test_loss", test_loss)
+        return {"test_loss": test_loss}
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+
+
+
+'''This is for setting a custom dataset for pytorch Dataloder
+    ref.:https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+'''
+class CustomMoleculeDataset(torch.utils.data.Dataset):
+    def __init__(self,smiles_string,properties_label):
+        self.smiles_string= smiles_string
+        self.properties_label = properties_label
+    def __len__(self):
+        return len(self.smiles_string)
+    def __getitem__(self, idx):
+        return self.smiles_string[idx], self.properties_label[idx]
+
+
+def split_validation_dataset(train_dataset,percentage_train):
+    '''
+     This is for building the train and validation datasets
+         - train_dataset:torch.utils.data.Dataset object
+         - percentage_train:percentage of training data default=0.9
+     - Returns:
+        -train_set_final: dataset for training
+        -valid_set_final: dataset for validation
+     '''
+    # use 20% of training data for validation
+    train_set_size = int(len(train_dataset) * percentage_train)
+    valid_set_size = len(train_dataset) - train_set_size
+    # split the train set into two
+    seed = torch.Generator().manual_seed(42)
+    train_set, valid_set = data.random_split(train_dataset, [train_set_size, valid_set_size], generator=seed)
+    train_set_final = torch.utils.data.DataLoader(train_set, batch_size=params['batch_size'])
+    valid_set_final = torch.utils.data.DataLoader(valid_set, batch_size=params['batch_size'])
+
+    return train_set_final,valid_set_final
+
+def main_property_run(params):
+    '''
+    This is for training process include property prediction
+        - Inputs: params (Use hyperparameters.load_params() to load params from expParam.json)
+    - Returns:
+        -train_set_final,
+        -valid_set_final,
+        -test_dataset,
+    '''
+    # load data
+    X_train, X_test, Y_train, Y_test, character_index_lookup_dict = load_dataset(params)
+    train_dataset = CustomMoleculeDataset(X_train, Y_train)
+    train_set_final, valid_set_final = split_validation_dataset(train_dataset, percentage_train=0.9)
+    test_dataset = CustomMoleculeDataset(X_test, Y_test)
+    test_set_final, _ = split_validation_dataset(test_dataset, percentage_train=1)
+    return train_set_final, valid_set_final, test_set_final
+
+
+def main_no_prop(params):
+    '''
+    This is for training process which not include property prediction
+        - Inputs: params (Use hyperparameters.load_params() to load params from expParam.json)
+    - Returns:
+        -train_set_final,
+        -valid_set_final,
+        -test_dataset,
+    '''
+    X_train, X_test, character_index_lookup_dict = load_dataset(params)
+    train_dataset = CustomMoleculeDataset(X_train[:500], Y_train[:500])
+    train_set_final, valid_set_final = split_validation_dataset(train_dataset, percentage_train=0.9)
+
+    test_dataset = CustomMoleculeDataset(X_test[:500], Y_test[:500])
+    return train_set_final, valid_set_final, test_dataset
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--exp_file',
+                        help='experiment file', default='expParam.json')
+    parser.add_argument('-d', '--directory',
+                        help='exp directory', default=None)
+    args = vars(parser.parse_args())
+    if args['directory'] is not None:
+        args['exp_file'] = os.path.join(args['directory'], args['exp_file'])
+
+    params = hyperparameters.load_params(args['exp_file'])
+    vae = MolecularVAE()
+    logger = CSVLogger("logs", name="my_exp_name")
+    train_set_final, valid_set_final, test_dataset = main_property_run(params)
+    #logger = TensorBoardLogger("tb_logs", name="my_model")
+    #trainer=pl.Trainer(fast_dev_run=True)
+    trainer = pl.Trainer(logger=logger)
+    trainer.fit(vae,train_dataloaders=train_set_final,val_dataloaders=valid_set_final)
+    val_result = trainer.test(vae, dataloaders=valid_set_final)
+    test_result = trainer.test(vae, dataloaders=test_dataset)
+
+
+   # print(vae(torch.rand(1,120,36)))
