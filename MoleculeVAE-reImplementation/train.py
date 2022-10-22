721821d296f46098d346b08b72f67065484c2d69
timcanby <kangyinglee0501@gmail.com>
2022-10-22 21:20:10 +0900

    10.22 re-write data_logger

--- MoleculeVAE-reImplementation/train.py
+++ MoleculeVAE-reImplementation/train.py
@@ -27,19 +27,56 @@ from scipy.spatial import geometric_slerp
 import time
 import os
 from datetime import datetime
-
+from recordclass import dataobject, astuple, asdict
 # dd/mm/YY H:M:S
 dt_string = datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S")
 print("date and time =", dt_string)
 
 
-'''Lists for recording experimental results'''
-#loss_record_list for recording the loss terms [epoch,vae_loss, reconstruction_loss, prediction_loss,process_type]
-record_list=[]
-#performance on separate prediction tasks [epoch,'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']
-separate_prediction_list=[]
-#list for record annealing performance [epoch,test_total_loss_with_annealing,test_total_loss_without_annealing,annealing_weights,process_type]
-list_for_record_annealing_performance=[]
+"---To record the Loss log---"
+
+
+class LogFile(object):
+    def __init__(self):
+        # loss_record_list for recording the loss terms [epoch,vae_loss, reconstruction_loss, prediction_loss,process_type]
+        self.record_list = []
+        # performance on separate prediction tasks [epoch,'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']
+        self.separate_prediction_list = []
+        # list for record annealing performance [epoch,test_total_loss_with_annealing,test_total_loss_without_annealing,annealing_weights,process_type]
+        self.list_for_record_annealing_performance = []
+
+    def add_record_list(self, log):
+        self.record_list.append(log)
+
+    def add_separate_prediction(self, log):
+        self.separate_prediction_list.append(log)
+
+    def add_list_for_record_annealing_performance(self, log):
+        self.list_for_record_annealing_performance.append(log)
+
+    def save_log(self,params):
+        if not os.path.exists('Loss_record'):
+            os.mkdir(dir_name)
+        '''separate_prediction_list For evaluate the prediction results on test set of each epoch'''
+        # performance on separate prediction tasks [epoch,'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']
+        pd.DataFrame(self.separate_prediction_list,
+                     columns=['epoch', 'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']).to_csv(
+            'Loss_record/' + str(dt_string.replace("/", '_')) + '_separate_prediction_list.csv')
+
+        '''list for record annealing performance '''
+        # [epoch,loss_with_annealing,loss_without_annealing,annealing_weights,process_type]
+        pd.DataFrame(self.list_for_record_annealing_performance,
+                     columns=['epoch', 'kl_loss_with_annealing', 'kl_without_annealing', 'annealing_weights',
+                              'process_type']).to_csv(
+            'Loss_record/' + str(dt_string.replace("/", '_')) + '_list_for_record_annealing_performance.csv')
+
+        ''' record_list  Only for VAE+ properties prediction task'''
+        # record_list for recording the loss terms [epoch,vae_loss, reconstruction_loss, prediction_loss,process_type]
+        if params['do_prop_pred']:
+            pd.DataFrame(self.record_list, columns=['epoch', 'vae_loss', 'reconstruction_loss', 'KL_loss', 'prediction_loss',
+                                               'process_type']).to_csv(
+                'Loss_record/' + str(dt_string.replace("/", '_')) + '_record_list.csv')
+
 
 
 
@@ -166,13 +203,12 @@ def vae_loss_with_annealing(x_decoded_mean, x, z_mean, z_logvar):
     vecloss=xent_loss (x_decoded_mean.to(dtype=torch.float32, device=device),x.to(dtype=torch.float32, device=device))
     kl_loss = -0.5 * torch.sum(1 + z_logvar - z_mean.pow(2) - z_logvar.exp())
     if epoch>=params['vae_annealer_start']:
-
         return vecloss,frange_cycle_cosine(0.0, 1.0, epoch, 4, 0.5)[0],kl_loss
     else:
         return vecloss,1,kl_loss
 
 
-def calculate_loss_by_type(decoder_output_data, input_string_data, z_mean, z_logvar, property_prediction_loss, epochs,process_type):
+def calculate_loss_by_type(decoder_output_data, input_string_data, z_mean, z_logvar, property_prediction_loss, epochs,process_type,logger):
     '''To calculate the loss value '''
     pre_loss = property_prediction_loss
     loss_type = params['loss_type']
@@ -187,7 +223,8 @@ def calculate_loss_by_type(decoder_output_data, input_string_data, z_mean, z_log
         vae_loss=vecloss+kl_loss
         loss=vae_loss + pre_loss
         #record_list.append([epochs, vae_loss, reconstruction_loss, prediction_loss, process_type])
-        record_list.append([epochs, vae_loss.cpu().detach().numpy(),vecloss.cpu().detach().numpy(),kl_loss.cpu().detach().numpy(), pre_loss.cpu().detach().numpy(), process_type])
+        logger.add_record_list([epochs, vae_loss.cpu().detach().numpy(),vecloss.cpu().detach().numpy(),kl_loss.cpu().detach().numpy(), pre_loss.cpu().detach().numpy(), process_type])
+
     elif loss_type == "vae_pre_with_annealing":
         vecloss,anealing_weight,kl_loss_original = vae_loss_with_annealing(decoder_output_data, input_string_data, z_mean, z_logvar)
         kl_loss=anealing_weight*kl_loss_original
@@ -196,10 +233,9 @@ def calculate_loss_by_type(decoder_output_data, input_string_data, z_mean, z_log
         loss = vae_loss + pre_loss
 
         # list for record annealing performance
-        list_for_record_annealing_performance.append([epochs,kl_loss.cpu().detach().numpy(),kl_loss_original.cpu().detach().numpy(),anealing_weight,process_type])
-        record_list.append( [epochs, vae_loss.cpu().detach().numpy(), vecloss.cpu().detach().numpy(), kl_loss.cpu().detach().numpy(),
+        logger.add_list_for_record_annealing_performance([epochs,kl_loss.cpu().detach().numpy(),kl_loss_original.cpu().detach().numpy(),anealing_weight,process_type])
+        logger.add_record_list([epochs, vae_loss.cpu().detach().numpy(), vecloss.cpu().detach().numpy(), kl_loss.cpu().detach().numpy(),
              pre_loss.cpu().detach().numpy(), process_type])
-
 
     else:
         loss = None
@@ -240,11 +276,12 @@ def plot3D(xi, yi, zi, c, label, title, n_min, n_max):
     # plt.axis('off')
     plt.title('Epoch=' + title)
 
-'''========================End of loss module=========================='''
+'''========================End of visualization module=========================='''
+
 
 '''Train & Test func.'''
 
-def train(epochs,train_set_final, valid_set_final):
+def train(epochs,train_set_final, valid_set_final,logger):
     model.train()
     train_loss = 0
     valid_loss = 0
@@ -256,7 +293,7 @@ def train(epochs,train_set_final, valid_set_final):
             output, mean, logvar,pre,z = model(data)
             pre_loss_ca=nn.MSELoss()
             pre_loss=pre_loss_ca(pre,label)
-            loss=calculate_loss_by_type(output,data, mean, logvar,pre_loss,epochs,'train')
+            loss=calculate_loss_by_type(output,data, mean, logvar,pre_loss,epochs,'train',logger)
             loss.backward()
             train_loss += loss
             optimizer.step()
@@ -267,14 +304,14 @@ def train(epochs,train_set_final, valid_set_final):
                         valid_output, valid_mean, valid_logvar, valid_pre, valid_z = model(valid_smidata.to(dtype=torch.float32, device=device))
                         valid_pre_loss_ca = nn.MSELoss()
                         valid_pre_loss = valid_pre_loss_ca(valid_pre, valid_labels.to(dtype=torch.float32, device=device))
-                        valid_loss+= calculate_loss_by_type(valid_output, valid_smidata, valid_mean, valid_logvar,valid_pre_loss, epochs,'valid')
+                        valid_loss+= calculate_loss_by_type(valid_output, valid_smidata, valid_mean, valid_logvar,valid_pre_loss, epochs,'valid',logger)
                     if params["SAVE_WEIGHT"]:
                         torch.save(model.state_dict(), 'Weights/' + str(dt_string.replace("/", '_')) + str(epochs) + '_param.pth')
 
                     print(f'{epochs} / {batch_idx}\t{loss:.4f}')
         return (train_loss.cpu().detach().numpy() / len(train_set_final)), (valid_loss.cpu().detach().numpy() / len(valid_set_final))
 
-def test(test_set,epochs):
+def test(test_set,epochs,logger):
     dataset=torch.utils.data.DataLoader(test_set, batch_size=params['batch_size'])
     test_loss=0
     with torch.no_grad():
@@ -283,27 +320,18 @@ def test(test_set,epochs):
             test_output, test_mean, test_logvar, test_pre, test_z = model(test_smidata.to(dtype=torch.float32, device=device))
             test_pre_loss_ca = nn.MSELoss()
             test_pre_loss = test_pre_loss_ca(test_pre, test_labels.to(dtype=torch.float32, device=device))
-            test_loss+=calculate_loss_by_type(test_output,test_smidata,test_mean,test_logvar,test_pre_loss,epochs,'test')
+            test_loss+=calculate_loss_by_type(test_output,test_smidata,test_mean,test_logvar,test_pre_loss,epochs,'test',logger)
             mae = nn.L1Loss()
             mae_logP = mae(test_labels [:, 0].to(device), test_pre[:, 0].to(device)).cpu().detach().numpy()
             mae_qed = mae(test_labels [:, 1].to(device), test_pre[:, 1].to(device)).cpu().detach().numpy()
             mae_SAS = mae(test_labels [:, 2].to(device), test_pre[:, 2].to(device)).cpu().detach().numpy()
 
-
             # performance on separate prediction tasks [epoch,'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']
-            # separate_prediction_list = []
-
-
-            separate_prediction_list.append(
-                [epochs,torch.mean((test_pre[:, 0])).cpu().detach().numpy(), torch.mean((test_pre[:, 1])).cpu().detach().numpy(),
+            logger.add_separate_prediction([epochs,torch.mean((test_pre[:, 0])).cpu().detach().numpy(), torch.mean((test_pre[:, 1])).cpu().detach().numpy(),
                  torch.mean((test_pre[:, 2])).cpu().detach().numpy(), mae_logP, mae_qed,
                  mae_SAS])
 
-
-
-
     return test_loss.cpu().detach().numpy()/len(dataset)
-
 
 
 if __name__ == "__main__":
@@ -324,29 +352,14 @@ if __name__ == "__main__":
 
     model = nn.DataParallel(MolecularVAE().to(device))
     optimizer = optim.Adam(model.parameters(), lr=params['lr'])
+    logger=LogFile()
 
     for epoch in range(1, epochs + 1):
         training_data_dic=load_data_by_task(params)
         train_set_final=training_data_dic['train_set_final'].to_numpy()
-        train_loss, valid_loss = train(epoch, training_data_dic['train_set_final'].tolist(), training_data_dic['valid_set_final'].tolist())
-        test_loss = test(training_data_dic['test_dataset'].tolist(), epoch)
+        train_loss, valid_loss = train(epoch, training_data_dic['train_set_final'].tolist(), training_data_dic['valid_set_final'].tolist(),logger)
+        test_loss = test(training_data_dic['test_dataset'].tolist(), epoch,logger)
         #print([epoch, train_loss, valid_loss, test_loss])
 
-    #separate_prediction_list For evaluate the prediction results on test set of each epoch
-    #performance on separate prediction tasks [epoch,'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']
-    pd.DataFrame(separate_prediction_list, columns=['epoch', 'logP_pre', 'qed_pre', 'SAS_pre', 'mae_logP', 'mae_qed', 'mae_SAS']).to_csv(
-        'Loss_record/' + str(dt_string.replace("/", '_')) + '_separate_prediction_list.csv')
-
-    #list for record annealing performance
-    #[epoch,loss_with_annealing,loss_without_annealing,annealing_weights,process_type]
-    pd.DataFrame(list_for_record_annealing_performance,
-                 columns=['epoch', 'kl_loss_with_annealing','kl_without_annealing','annealing_weights','process_type']).to_csv(
-        'Loss_record/' + str(dt_string.replace("/", '_')) + '_list_for_record_annealing_performance.csv')
-
-
-    #record_list  Only for VAE+ properties prediction task
-    # record_list for recording the loss terms [epoch,vae_loss, reconstruction_loss, prediction_loss,process_type]
-    if params['do_prop_pred']:
-        pd.DataFrame(record_list, columns=['epoch','vae_loss', 'reconstruction_loss', 'KL_loss','prediction_loss','process_type']).to_csv('Loss_record/'+str(dt_string.replace("/",'_'))+'_record_list.csv')
-
+    logger.save_log(params)
 
